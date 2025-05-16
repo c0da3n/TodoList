117ca5c5423442baa21d7725760af7ab225f9614
c0da3n <sky@codawn.dev>
2025-05-16 15:58:52 -0400

    feat: Create

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const mockData = [
   {
@@ -27,10 +27,24 @@ const mockData = [
 
 const App = () => {
   const [todos, setTodos] = useState(mockData);
+
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos((prev) => {
+      return [newTodo, ...prev];
+    });
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
